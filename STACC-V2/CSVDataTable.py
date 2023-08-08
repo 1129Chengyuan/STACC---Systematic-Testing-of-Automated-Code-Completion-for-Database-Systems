4d5ca4ade24d011a890e1c00a4bab247af0f09c9
1129Chengyuan <1129chengyuan@gmail.com>
2023-08-09 03:14:00 +0800

    Added readme; converted code generated to UTF-8 to avoid compilation errors, formatted generated code; fixed issue w/ python file not reloading every iteration; integrated flags from the bash file

---
+++ STACC-V2/CSVDataTable.py
@@ -0,0 +1,22 @@
+import copy
+import csv
+
+
+class CSVDataTable:
+    """
+    The implementation classes (XXXDataTable) for CSV database, relational, etc. with extend the
+    base class and implement the abstract methods.
+    """
+
+    def __init__(self, table_name, connect_info, key_columns):
+        """
+
+        :param table_name: Logical name of the table.
+        :param connect_info: Dictionary of parameters necessary to connect to the data.
+        :param key_columns: List, in order, of the columns (fields) that comprise the primary key.
+        """
+        self.table_name = table_name
+        self.connect_info = connect_info
+        self.key_columns = key_columns
+        # pass
+
